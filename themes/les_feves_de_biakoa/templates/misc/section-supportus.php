63897228541f86772498bbcc4f4f65a6a32ea2e6
jojo36 <jbodan@orange.fr>
2017-11-30 15:56:33 +0100

    support paypal account, fix action page

--- themes/les_feves_de_biakoa/templates/misc/section-supportus.php
+++ themes/les_feves_de_biakoa/templates/misc/section-supportus.php
@@ -9,7 +9,7 @@
                 <h3 class="supportUs__title"><?php echo get_field( 'section_1_don_sous_titre' ); ?></h3>
                 <p class="supportUs__paragraph"><?php echo get_field( 'section_1_don_texte' ); ?></p>
                 <div class="txt-center">
-                    <a class="btn btn--dark" href="#">Faire un don</a>
+                    <a class="btn btn--dark" href="<?php echo get_option('setting_paypal'); ?>">Faire un don</a>
                 </div>
             </div>
             <div class="supportUs__img">

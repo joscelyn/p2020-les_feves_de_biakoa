10009b7e8d44c6198d3a60e8b0388f56c92e0c68
joscelyn <joscelyn.gainie@gmail.com>
2017-12-01 10:54:56 +0100

    Add header default thumbnail

--- themes/les_feves_de_biakoa/header.php
+++ themes/les_feves_de_biakoa/header.php
@@ -14,10 +14,10 @@
 <body class="<?php if( is_front_page() ): echo 'home'; else: echo $slug = basename(get_permalink()); endif;?>" <?php body_class(); ?>>
 
 <div class="headerBackground">
-    <?php if (has_post_thumbnail()) : ?>
+    <?php if (has_post_thumbnail()): ?>
         <img src="<?php the_post_thumbnail(); ?>" alt="<?php the_post_title(); ?>">
     <?php else: ?>
-        <img src="<?= IMAGES_URL ?>/home.jpg" alt="<?php the_post_title(); ?>">
+        <img src="<?= IMAGES_URL ?>/home.jpg" alt="">
     <?php endif; ?>
 </div>
 

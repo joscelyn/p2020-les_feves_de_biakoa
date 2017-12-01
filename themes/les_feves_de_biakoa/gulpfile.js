63e181ea65362524b1a1e7422034f7aed218b91f
joscelyn <joscelyn.gainie@gmail.com>
2017-12-01 08:44:30 +0100

    Gulp tasks and minify

--- themes/les_feves_de_biakoa/gulpfile.js
+++ themes/les_feves_de_biakoa/gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp'),
 	plumber = require('gulp-plumber'),
 	sass = require('gulp-sass'),
-	webserver = require('gulp-webserver'),
 	autoprefixer = require('gulp-autoprefixer'),
 	cssmin = require('gulp-cssmin'),
     imagemin = require('gulp-imagemin'),
@@ -9,85 +8,56 @@ var gulp = require('gulp'),
     rename = require("gulp-rename");
 
 
+// Watch tasks
 
 gulp.task('cssCompile', function () {
-	gulp.src('./css/**/*.scss')
+	gulp.src('./dist/css/sass/**/*.scss')
 		.pipe(plumber())
 		.pipe(sass())
-		// A cause d'un bug lié au src, on utilise gulp-rename pour clear le path
-		.pipe(rename(function (path) {
-			path.dirname = "/css";
-		}))
+		.pipe(rename('style.css'))
 		.pipe(gulp.dest('./'));
 });
 
-gulp.task('webserver', function() {
-	gulp.src('./')
-		.pipe(webserver({
-			host: 'localhost',
-			port: '1111',
-			livereload: true,
-			directoryListing: false,
-			open: true
-		}));
-});
 
 gulp.task('watch', function(){
-	gulp.watch('./css/**/*.scss', ['cssCompile']);
+	gulp.watch('./dist/css/sass/**/*.scss', ['cssCompile']);
 });
 
+gulp.task('default', ['cssCompile', 'watch']);
 
 
-gulp.task('default', ['cssCompile', 'webserver', 'watch']);
 
 
-
+// Prod tasks
 
 gulp.task('cssMin', function () {
-	gulp.src('./css/**/*.scss')
+	gulp.src('./dist/css/sass/**/*.scss')
 		.pipe(plumber())
 		.pipe(sass())
         .pipe(autoprefixer({
-            browsers: ['> 0.01% in FR'], // a voir selon le poid du fichier
-            remove: false,
-            grid: true // peut etre a modifier ?
+            browsers: ['> 0.1% in FR'],
+            remove: false
         }))
 		.pipe(cssmin())
-		.pipe(rename(function (path) {
-			path.dirname = "/css";
-		}))
+		.pipe(rename('style.css'))
 		.pipe(gulp.dest('./'));
 });
 
 
-
-
-
-
-// Pas actualisé
-
-gulp.task('style', function () {
-	gulp.src('./app/css/**/*.scss')
-		.pipe(plumber())
-		.pipe(sass())
-		.pipe(autoprefixer())
-		.pipe(cssmin())
-		.pipe(gulp.dest('./style.css'));
-});
-
-gulp.task('imagemin', function () {
-    gulp.src('./app/img/**/*.*')
+gulp.task('imgMin', function () {
+    gulp.src('./dist/img/**/*.*')
         .pipe(imagemin({
             progressive: true,
             use: [imageminPngquant()]
         }))
-        .pipe(gulp.dest('./prod/img'));
+        .pipe(gulp.dest('./dist/img'));
 });
 
-gulp.task('html', function() {
-	gulp.src('./app/**/*.html')
-	.pipe(gulp.dest('./prod'));
+gulp.task('imagesMin', function () {
+    gulp.src('./dist/images/**/*.*')
+        .pipe(imagemin({
+            progressive: true,
+            use: [imageminPngquant()]
+        }))
+        .pipe(gulp.dest('./dist/images'));
 });
-
-
-gulp.task('prod', ['style', 'imagemin', 'html']);

6def06ee0bff85221e261b850b889f7f1ffcc3c6
joscelyn <joscelyn.gainie@gmail.com>
2017-12-01 11:11:53 +0100

    change header size

--- themes/les_feves_de_biakoa/header.php
+++ themes/les_feves_de_biakoa/header.php
@@ -15,7 +15,7 @@
 
 <div class="headerBackground">
     <?php if (has_post_thumbnail()): ?>
-        <img src="<?php the_post_thumbnail(); ?>" alt="">
+        <?php the_post_thumbnail(); ?>
     <?php else: ?>
         <img src="<?= IMAGES_URL ?>/home.jpg" alt="">
     <?php endif; ?>

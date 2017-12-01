0c1105c478463a40a0a78f9cb0d3187e7975e9ac
joscelyn <joscelyn.gainie@gmail.com>
2017-12-01 10:44:06 +0100

    Add 404

---
+++ themes/les_feves_de_biakoa/404.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+<main>
+    <div class="headTitles">
+        <h2 class="headTitles__sub">Erreur 404</h2>
+        <h1 class="headTitles__main">Cette page n'existe pas</h1>
+    </div>
+</main>
+</div>
+
+<?php get_footer(); ?>
